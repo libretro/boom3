52ed76a6bbd66d083b867f34d38aa87aa1ef10dc
twinaphex <libretro@gmail.com>
2019-11-03 15:53:00 +0100

    Revert "Comment out L2/R2 quicksave/load for now"
    This reverts commit 9fd79620d55e321354d6c566bc3621cf58722be5.

--- neo/sys/libretro/libretro.cpp
+++ neo/sys/libretro/libretro.cpp
@@ -547,7 +547,6 @@ void Sys_SetKeys(){
 				Key_Event(K_ENTER, 1);
 			else if (!(ret & (1 << RETRO_DEVICE_ID_JOYPAD_R)) && (old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_R)))
 				Key_Event(K_ENTER, 0);
-#if 0
 			if ((ret & (1 << RETRO_DEVICE_ID_JOYPAD_L2)) && !(old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_L2)))
 				Key_Event(K_AUX1, 1);
 			else if (!(ret & (1 << RETRO_DEVICE_ID_JOYPAD_L2)) && (old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_L2)))
@@ -556,8 +555,6 @@ void Sys_SetKeys(){
 				Key_Event(K_AUX2, 1);
 			else if (!(ret & (1 << RETRO_DEVICE_ID_JOYPAD_R2)) && (old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_R2)))
 				Key_Event(K_AUX2, 0);
-#endif
-
 			if ((ret & (1 << RETRO_DEVICE_ID_JOYPAD_L3)) && !(old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_L3)))
 				Key_Event(K_AUX3, 1);
 			else if (!(ret & (1 << RETRO_DEVICE_ID_JOYPAD_L3)) && (old_ret & (1 << RETRO_DEVICE_ID_JOYPAD_L3)))
